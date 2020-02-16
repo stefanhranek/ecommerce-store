3f1ad5ff8109d1f4f1e38c1d6e19a5d0bf4b44ed
stefan hranek <hranekstefan@gmail.com>
2020-02-15 23:25:32 -1000

    Index - redux setup; wrapped App with Provider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import Client from 'shopify-buy';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Client from "shopify-buy";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const client = Client.buildClient({
-    storefrontAccessToken: 'your-access-token',
-    domain: 'your-shopify-url.myshopify.com'
+  storefrontAccessToken: "your-access-token",
+  domain: "your-shopify-url.myshopify.com"
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App locale={locale} />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
