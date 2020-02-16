16cdb9340af43f745cbc38ea5043703b7c555f75
stefan hranek <hranekstefan@gmail.com>
2020-02-15 23:21:00 -1000

    Installed react-redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import Shop from './pages/Shop';
 import ProductDetails from './pages/ProductDetails';
-import Cart from './pages/Cart';
 import Contact from './pages/Contact';
+
+import Cart from './components/shopify/Cart';
+import { render } from '@testing-library/react';
  
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Shop}></Route>
-          <Route exact path="/details" component={ProductDetails}></Route>
-          <Route exact path="/cart" component={Cart}></Route>
-          <Route exact path="/contact" component={Contact}></Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.updateQuantityInCart = this.updateQuantityInCart.bind(this);
+    this.removeLineItemInCart = this.removeLineItemInCart.bind(this);
+    this.handleCartClose = this.handleCartClose.bind(this);
+  }
+  render() {  
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Shop}></Route>
+            <Route exact path="/details" component={ProductDetails}></Route>
+            <Route exact path="/cart" component={Cart}></Route>
+            <Route exact path="/contact" component={Contact}></Route>
+          </Switch>
+        </BrowserRouter>
+        <Cart/>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
